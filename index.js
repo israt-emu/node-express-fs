30774c5fd7b6e8287a3109f4cdeea899c024ca4d
unknown <isratjahan.emu1998@gmail.com>
2022-10-12 23:33:51 +0600

    2nd

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 const usersRoute = require("./routes/users.route");
 app.use(express.json());
 //using user router
